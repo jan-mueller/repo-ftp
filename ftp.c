19a9c40d9fc6ec9244514503358df397473570fd
Jan Müller <jan@t420.(none)>
2012-08-01 19:57:05 +0200

    Initial Commit

---
+++ ftp.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "ftp.h"
+
+
+int abor(){
+}
+int acct(){
+}
+int allo(){
+}
+int appe(){
+}
+int cdup(){
+}
+int cwd(){
+}
+int dele(){
+}
+int help(){
+}
+int list(){
+}
+int mkd(){
+}
+int mode(){
+}
+int nlst(){
+}
+int noop(){
+}
+int pass(){
+}
+int pasv(){
+}
+int port(){
+}
+int pwd(){
+}
+int quit(){
+}
+int rein(){
+}
+int rest(){
+}
+int retr(){
+}
+int rmd(){
+}
+int rnfr(){
+}
+int rnto(){
+}
+int site(){
+}
+int smnt(){
+}
+int stat(){
+}
+int stor(){
+}
+int stou(){
+}
+int stru(){
+}
+int syst(){
+}
+int type(){
+}
+int user(){
+}
---
+++ ftp.h
@@ -0,0 +1,33 @@
+#ifndef _FTP_H_
+
+#define _FTP_H_
+
+
+
+int abor();
+int acct();
+int allo();
+int appe();
+int cdup();
+int cwd();
+int dele();
+int help();
+int list();
+int mkd();
+int mode();
+int nlst();
+int noop();
+int pass();
+int pasv();
+int port();
+int pwd();
+int quit();
+int rein();
+int rest();
+int retr();
+int rmd();
+int rnfr();
+int rnto();
+
+
+#endif
