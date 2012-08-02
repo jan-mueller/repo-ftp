0aa5a4aaa8a5c011e7af251b7edc92708fd57d23
Jan Müller <jan.mueller.online@googlemail.com>
2012-08-02 20:22:23 +0200

    Defined Command types

--- ftp.h
+++ ftp.h
@@ -4,30 +4,35 @@
 
 
 
-int abor();
-int acct();
-int allo();
-int appe();
-int cdup();
-int cwd();
-int dele();
-int help();
-int list();
-int mkd();
-int mode();
-int nlst();
-int noop();
-int pass();
-int pasv();
-int port();
-int pwd();
-int quit();
-int rein();
-int rest();
-int retr();
-int rmd();
-int rnfr();
-int rnto();
+/* FTP Command tokens */
+enum command_types{ 
+	USER, PASS, ACCT, CWD, CDUP, SMNT, REIN, QUIT, PORT, PASV, TYPE, STRU, MODE, 
+	RETR, STOR, STOU, APPE, ALLO, REST, RNFR, RNTO, ABOR, DELE, RMD, MKD, PWD, 
+	LIST, NLST, SITE, SIZE, SYST, STAT, HELP, NOOP, EOFC, ERROR,
+};
+
+/* struct used to build a table mapping command character sequences to
+ *    their command token listed above */
+struct command_list{
+	char *command;
+	enum command_types command_code;
+};
+
+/* Lookup table for FTP Commands*/
+struct command_list command_lookup[] = {
+	{"USER", USER}, {"PASS", PASS}, {"ACCT", ACCT}, {"CWD", CWD},
+	{"CDUP", CDUP}, {"SMNT", SMNT}, {"REIN", REIN}, {"QUIT", QUIT},
+	{"PORT", PORT}, {"PASV", PASV}, {"TYPE", TYPE}, {"STRU", STRU},
+	{"MODE", MODE}, {"RETR", RETR}, {"STOR", STOR}, {"STOU", STOU},
+	{"APPE", APPE}, {"ALLO", ALLO}, {"REST", REST}, {"RNFR", RNFR},
+	{"RNTO", RNTO}, {"ABOR", ABOR}, {"DELE", DELE}, {"RMD", RMD},
+	{"MKD", MKD}, {"XMKD", MKD}, {"XRMD", RMD}, {"PWD", PWD},
+	{"XPWD", PWD}, {"LIST", LIST}, {"NLST", NLST}, {"SITE", SITE},
+	{"SIZE", SIZE}, {"SYST", SYST}, {"STAT", STAT}, {"HELP", HELP},
+	{"NOOP", NOOP}, {"EOFC", EOFC},
+};
+
+enum command_modes {LOGIN, PASSWORD, COMMANDS, EXIT};
 
 
 #endif
