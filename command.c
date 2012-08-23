b5fa2b93842a6bf3101a7db6755e0c82bedca6ef
Jan Müller <jan.mueller.online@googlemail.com>
2012-08-23 18:08:21 +0200

    command.c command.h added

---
+++ command.c
@@ -0,0 +1,56 @@
+/*********************************************************
+ * File:	 command.c
+ * Author: Jan Müller
+ * 
+ * Created: Aug 23 2012 17:50:49
+ * Last Changes: Aug 23 2012 18:06:39
+ *
+ * E-mail: jan.mueller.online@gmail.com 
+ * Description:
+ *
+ *********************************************************/
+
+#include "command.h"
+ 
+int abor();
+
+int cwd();
+
+int dele();
+
+int list();
+
+int mdtm();
+
+int mkd();
+
+int nlst();
+
+int pass();
+
+int pasv();
+
+int port();
+
+int pwd();
+
+int quit();
+
+int retr();
+
+int rmd();
+
+int rnfr();
+
+int rnto();
+
+int site();
+
+int size();
+
+int stor();
+
+int type();
+
+int user();
+
---
+++ command.h
@@ -0,0 +1,54 @@
+#ifndef _COMMAND_H_
+
+#define _COMMAND_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int abor();
+
+int cwd();
+
+int dele();
+
+int list();
+
+int mdtm();
+
+int mkd();
+
+int nlst();
+
+int pass();
+
+int pasv();
+
+int port();
+
+int pwd();
+
+int quit();
+
+int retr();
+
+int rmd();
+
+int rnfr();
+
+int rnto();
+
+int site();
+
+int size();
+
+int stor();
+
+int type();
+
+int user();
+
+
+#endif
